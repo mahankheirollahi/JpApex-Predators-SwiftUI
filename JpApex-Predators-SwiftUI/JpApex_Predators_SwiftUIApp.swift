e70d417860011f93f7435b3419ac69e28912221b
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-12-24 22:14:02 +0300

    Initial Commit

---
+++ JpApex-Predators-SwiftUI/JpApex_Predators_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JpApex_Predators_SwiftUIApp.swift
+//  JpApex-Predators-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct JpApex_Predators_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
