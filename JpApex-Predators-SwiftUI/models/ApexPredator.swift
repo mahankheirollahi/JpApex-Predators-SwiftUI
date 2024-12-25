5eb0f9fa9abf3cbdb110ed6931e7560d911c789c
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-12-25 15:46:22 +0300

    Add project models

---
+++ JpApex-Predators-SwiftUI/models/ApexPredator.swift
@@ -0,0 +1,19 @@
+//
+//  ApexPredator.swift
+//  JpApex-Predators-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 12/25/24.
+//
+
+import Foundation
+
+struct ApexPredator: Decodable {
+    let id: Int
+    let name: String
+    let type: String
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movie_scenes: [MovieScene]
+    let link: String
+}
---
+++ JpApex-Predators-SwiftUI/models/MovieScene.swift
@@ -0,0 +1,14 @@
+//
+//  MovieScene.swift
+//  JpApex-Predators-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 12/25/24.
+//
+
+import Foundation
+
+struct MovieScene: Decodable {
+    let id: Int
+    let movie: String
+    let scene_description: String
+}
